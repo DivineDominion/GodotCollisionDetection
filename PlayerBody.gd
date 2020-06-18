0e7d0f27c935df2256312e79c6c466f90ca1e2fb
Christian Tietze <christian.tietze@gmail.com>
2020-06-18 19:52:19 +0200

    restrict movement to X axis

--- PlayerBody.gd
+++ PlayerBody.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var speed: float = 200
+var speed: float = 1800
 var velocity: Vector2 = Vector2()
 onready var screen_size: Vector2 = get_viewport_rect().size
 
@@ -10,10 +10,6 @@ func _input(event: InputEvent) -> void:
         self.velocity.x = 1
     if Input.is_action_pressed("ui_left"):
         self.velocity.x = -1
-    if Input.is_action_pressed("ui_down"):
-        self.velocity.y = 1
-    if Input.is_action_pressed("ui_up"):
-        self.velocity.y = -1
 
 func _physics_process(delta: float) -> void:
     if velocity.length() > 0:
